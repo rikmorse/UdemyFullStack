a133136bec5ec450b2d104f3faadda1078730a3b
R Morse <rikmorse@gmail.com>
2025-07-10 14:05:21 -0700

    Updated Sec 14 JS
    JS course mainly in devtools so not code to commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* var dogAge = prompt("Please enter the age of your dog to find their human age!");
+var humanAge = ((dogAge - 2) * 4 + 21);
+alert ("You dog is " + humanAge + " human years old!"); */
+
+function getMilk() {
+    console.log("Leave house");
+    console.log("Move right");
+    console.log("Move right");
+    console.log("Move right");
+    console.log("Buy Milk");
+    console.log("Move left");
+    console.log("Move left");
+    console.log("Move left");
+    console.log("Return home");
+}
+getMilk();
