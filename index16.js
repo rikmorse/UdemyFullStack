8c1f9136633ab8bb03e09f2a6c388aa2d09bf237
R Morse <rikmorse@gmail.com>
2025-09-12 10:19:14 -0700

    more JS learning devtools
    Manipulating the DOM through dev tools

--- index16.js
+++ index16.js
@@ -1,2 +1,12 @@
-// alert('Hello JavaScript');
-//document.querySelector("h1").innerHTML = "This may work!";
+// alert("Hello this is an alert!!!");
+// document.querySelector("h1").innerText = "Hello, from JS file!";
+
+// This will change the third li element text in dev tools console.
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Richard";/*  */
+
+// This change will modify the color of the third li after selecting all li elements.
+/* document.getElementsByTagName("li"); */ //Find all li elements.
+/* document.getElementsByTagName("li")[2].style.color = "purple"; */ //Change the color of the third li element.
+
+/* This will show you the length of all items under said tag */
+// document.getElementsByTagName("li").length;
