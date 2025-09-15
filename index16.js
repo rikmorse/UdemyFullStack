d447f35a23106fa4a17a99248dbc277de3776c75
R Morse <rikmorse@gmail.com>
2025-09-15 16:11:12 -0700

    JS DOM modification through dev tools
    Dev Tools changing the DOM

--- index16.js
+++ index16.js
@@ -10,3 +10,32 @@
 
 /* This will show you the length of all items under said tag */
 // document.getElementsByTagName("li").length;
+
+/* Class name dev tools */
+/* document.getElementsByClassName("btn");
+document.getElementsByClassName("btn")[0].style.color = "green"; */
+
+/* Get by ID */
+/* document.getElementById("checkBox"); */
+/* document.getElementById("checkBox").checked = true; */  //This will check the checkbox.
+
+
+/* querySelector can pick any as long as the . # or tag is referenced correctly */
+/* document.querySelector(".list");
+<li class=​"list">​…​</li>​
+
+document.querySelector("#checkBox");
+<input id=​"checkBox" type=​"checkbox">​
+
+document.querySelector("button");
+<button class=​"dropbtn">​…​</button>​ 
+
+document.querySelector("li a");*/
+// multiple elements can be selected this way. This will select the first li a element.
+
+/* selector and class */
+//document.querySelector("li.list");
+/* tag and id */
+//document.querySelector("input#checkBox");
+/* class and tag */
+//document.querySelector(".list a");
