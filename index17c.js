f402adf03634b070f2171b0f6070f2f9e120fcf7
R Morse <rikmorse@gmail.com>
2025-09-24 16:11:36 -0700

    Finished Section 17 challenge
    Added JS for random dice roll and selecting correct image to display after refresh is hit.
    Added class to HTML h1 for clarification.

--- index17c.js
+++ index17c.js
@@ -1,17 +1,20 @@
-document.query
 
-function rollDice(dice1, dice2) {Selector(".img1").setAttribute("src", "./images/DICE/dice3.png");
+function rollDice(dice1, dice2) {
     dice1 = Math.floor(Math.random() * 6) + 1;
     dice2 = Math.floor(Math.random() * 6) + 1;
     console.log(dice1);
     console.log(dice2);
+    document.querySelector(".img1").setAttribute("src", "./images/DICE/dice" + dice1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "./images/DICE/dice" + dice2 + ".png");
     if (dice1 > dice2) {
         document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
     } else if (dice2 > dice1) {
         document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
     } else {
-        document.querySelector("h1").innerHTML = "It's a Draw!";
-    }
-document.querySelector(".img2").setAttribute("src", "./images/DICE/dice4.png");Selector(".img2").setAttribute("src", "./images/DICE/dice4.png");Selector(".img1").setAttribute("src", "./images/DICE/dice3.png");Selector(".img2").setAttribute("src", "./images/DICE/dice4.png");
-document.querySelector(".img1").setAttribute("src", "./images/DICE/dice3.png");Selector(".img2").setAttribute("src", "./images/DICE/dice4.png");
+        document.querySelector("h1").innerHTML = "It's a Draw! Try again!!!";
+    }   
+    return [dice1, dice2];
 }
+rollDice();
+
+
