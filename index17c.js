eadd531debc56ff200587bd2c1c94efe7b0c98c0
R Morse <rikmorse@gmail.com>
2025-09-26 21:58:11 -0700

    update JS challenge 17
    added instructor code

--- index17c.js
+++ index17c.js
@@ -18,3 +18,22 @@
 rollDice(); */
 
 
+/* Solution Instructor version */
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // random number between 1 and 6.
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
+var randomImageSource = "./images/DICE/" + randomDiceImage; // images/dice1.png - images/dice6.png
+image1.setAttribute("src", randomImageSource);
+
+/* var randomNumber2 = Math.floor(Math.random() * 6) + 1; */ // random number between 1 and 6.
+/* var randomImageSource2 = "./images/DICE/dice" + randomNumber2 + ".png"; */ // images/dice1.png - images/dice6.png
+/* document.querySelectorAll("img").setAttribute("src", randomImageSource2); */
+
+// If player 1 wins
+/* if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";   
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";   
+} else {
+    document.querySelector("h1").innerHTML = "It's a Draw! Try again!!!";    
+}
+    */
